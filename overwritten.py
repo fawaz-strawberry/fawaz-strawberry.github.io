06975ef877b2fbc8131872fed3a54eb9076d8916
Fawaz <fawazgames223@gmail.com>
2020-08-13 14:50:21 -0700

    Updated Tickers
    Apparently We're Selling Everything

--- overwritten.py
+++ overwritten.py
@@ -9,7 +9,7 @@ import base64
 data = open("C:/Users/Fawaz Mujtaba/Documents/GitHub/fawaz-strawberry.github.io/stock_data.js", "rb").read()
 encoded = base64.b64encode(data)
 print(str(encoded))
-encoded = json.dumps(str(encoded))
+encoded = json.dumps(encoded.decoded
 #DATA = {"message":"Updated Tickers", "content":encoded}
 
 r = requests.put(url=BASE_URL, data=encoded, headers=HEADERS, auth=("fawaz-strawberry", "3933c72e12d9f89bcbd608579b0717ac6f883163"))

e6f3101abaa6c94027bee81fe11f62b4cd7234f4
Fawaz <fawazgames223@gmail.com>
2020-09-08 18:37:26 -0700

    auto push

--- projects.js
+++ projects.js
@@ -3,12 +3,12 @@ site_data = {
                 [
                     {
                         "Title": "Cider Truck",
-                        "Description": "cider.txt",
+                        "Description": "Cider Truck is a fun and exciting mobile game where players race through the streets avoiding incoming obstacles such as busses, cars, trucks, planes, and more!",
                         "Image": "cider_truck.png"
                     },
                     {
                         "Title": "Stock AI",
-                        "Description": "stonk.txt",
+                        "Description": "The Stock Market AI is capable of determining buy and sell moments right before they become a trend eliminating emotional decision making from humans.",
                         "Image": "stonks.png"
                     }
                 ]

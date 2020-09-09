8e2c594e75a5c58cc7515aac8587027de5ce2853
Fawaz <fawazgames223@gmail.com>
2020-09-08 18:15:53 -0700

    Warm up round

---
+++ projects.js
@@ -0,0 +1,15 @@
+site_data = {
+                "Projects":
+                [
+                    {
+                        "Title": "Cider Truck",
+                        "Description": "cider.txt",
+                        "Image": "cider_truck.png"
+                    },
+                    {
+                        "Title": "Stock AI",
+                        "Description": "stonk.txt",
+                        "Image": "stonks.png"
+                    }
+                ]
+            }

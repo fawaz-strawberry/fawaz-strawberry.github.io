19d8f5fbe334b7ed64ae2c6680202e36a247bc87
Fawaz <fawazgames223@gmail.com>
2021-02-14 11:12:13 -0800

    auto push

--- ReactStuff/src/App.js
+++ ReactStuff/src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hi, my name is Fawaz Mujtaba, I'm currently rebuilding/updating
-         <br></br>
-         my website but I am available to be emailed at fawazgames223(at)gmail(dot)com!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      
     </div>
   );
 }
---
+++ ReactStuff/src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Demos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Resume',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Music',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'AI',
+        url: '#',
+        cName: 'nav-links'
+    },
+]

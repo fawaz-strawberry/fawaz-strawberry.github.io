ecab6e2bb2ae50babe35dd7916885f7326bc40d3
Fawaz <fawazgames223@gmail.com>
2021-08-30 09:45:52 -0700

    auto push

---
+++ site_data.js
@@ -0,0 +1,10 @@
+
+mega_holder = {
+    "research":[{"title":"this is research"}, {"date":"12/20/1999"}, {"subtitle":"section1"}, {"information":"random insightful information"}, {"image":"NN_Creator.PNG"}, {"image":"spectogram.PNG"}]
+}
+
+
+function getJSON(title)
+{
+    return mega_holder[title]
+}
